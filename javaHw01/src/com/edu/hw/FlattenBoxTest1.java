792f6c019f374b83bd30ab50820106a98bb2b927
JoWonHyeung <mesh153@naver.com>
2023-07-06 17:30:52 +0900

    update hw01

--- javaHw01/src/com/edu/hw/FlattenBoxTest1.java
+++ javaHw01/src/com/edu/hw/FlattenBoxTest1.java
@@ -9,24 +9,46 @@ public class FlattenBoxTest1 {
 		Scanner sc = new Scanner(System.in);
 		int tc = 10;
 		int boxLen = 100;
-		for(int tcn = 1; tcn<=tc; tcn++) {
+		for(int tcn = 1; tcn <= tc; tcn++) {
 			int dump = sc.nextInt();
 			int[ ] boxes = new int[boxLen];
 			for(int i=0; i<boxes.length; i++) {
 				boxes[i] = sc.nextInt();
 			}//초기화
 			
-			int heighDiff=flatten(boxes,dump );
+			int heighDiff = flatten(boxes,dump);
 			System.out.printf("#%d %d%n",tcn, heighDiff);
 		}
 
 	}
 
 	static int flatten(int[] boxes, int dump) {
+		int[] boxCnt = new int[101];
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
 		
-		// 코드구현
+		for(int num : boxes) {
+			boxCnt[num]++;
+			if(max < num) max = num;
+			if(min > num) min = num;
+		}
 		
-		return 00;
+		for(int i = 0; i < dump; i++) {
+			//dump작업
+			boxCnt[max]--;
+			boxCnt[max - 1]++;
+			
+			boxCnt[min]--;
+			boxCnt[min + 1]++;
+			
+			if(boxCnt[max] == 0) max -= 1;
+			if(boxCnt[min] == 0) min += 1;
+			
+			//차이가 1일 때는 dump횟수가 남아있더라도 탈출한다.
+			if(max - min == 1) break;
+		}
+		
+		return max - min;
 	}
 
 }
