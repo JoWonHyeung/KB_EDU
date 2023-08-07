165fc0e31cee7586f9ca6e684dff0d157d1eb275
JoWonHyeong <mesh153@naver.com>
2023-08-07 23:42:50 +0900

    .

--- work021_sql/09_jdbc04.sql
+++ work021_sql/09_jdbc04.sql
@@ -41,70 +41,80 @@ SELECT * FROM shares;
 
 /////////////////////////////////////////////////////
 
-create table company
-       (name varchar(255) primary key,
-       address varchar(255));
+--create table company
+--       (company_name varchar(255) primary key,
+--       address varchar(255));
        
 create table card
        (card_id varchar(255) primary key,
-       customer_id varchar(255),
-       expire date,
-       name varchar(255),
-       grade number);
+       expire_date date,
+       company_name varchar(255),
+       cust_grade varchar(255));
        
-create table customer
-       (customer_id varchar(255) primary key,
-       name varchar(255),
-       address varchar(255),
+create table cust
+       (ssn varchar(255) primary key,
+       cust_name varchar(255),
+       cust_address varchar(255),
        phone varchar(255));
        
 create table purchase
        (id number primary key,
-       customer_id varchar(255),
+       ssn varchar(255),
        card_id varchar(255),
        day date,
        price number,
+       company_name varchar(255),
        category varchar(255));
     
 drop table purchase;
 drop table card;
-drop table customer;
-drop table company;
+drop table cust;
+//drop table company;
 
-ALTER TABLE card ADD CONSTRAINT fk_card_customer_id foreign key(customer_id) references customer(customer_id) ON DELETE CASCADE;
-ALTER TABLE card ADD CONSTRAINT fk_card_company_name foreign key(name) references company(name) ON DELETE CASCADE;
 
-ALTER TABLE purchase ADD CONSTRAINT fk_purchase_customer_id foreign key(customer_id) references customer(customer_id) ON DELETE CASCADE;
+//ALTER TABLE card ADD CONSTRAINT fk_card_company_name foreign key(company_name) references company(company_name) ON DELETE CASCADE;
+
+ALTER TABLE purchase ADD CONSTRAINT fk_purchase_cust_id foreign key(ssn) references cust(ssn) ON DELETE CASCADE;
 ALTER TABLE purchase ADD CONSTRAINT fk_purchase_card_id foreign key(card_id) references card(card_id) ON DELETE CASCADE;
 
 CREATE SEQUENCE seq_purchase
 START WITH 1
 INCREMENT BY 1;
 
-truncate table company; 
 truncate table card;
 truncate table purchase;
-truncate table customer;
+truncate table cust;
+--
+--INSERT INTO company VALUES ('»ï¼º','aaa');
+--INSERT INTO company VALUES ('·Ôµ¥','bbb');
+--select * from company;
 
-INSERT INTO company VALUES ('»ï¼º','aaa');
-INSERT INTO company VALUES ('·Ôµ¥','bbb');
-select * from company;
+INSERT INTO cust VALUES ('mesh153','Á¶¿øÇü','23/01/23','01036493276');
+INSERT INTO cust VALUES ('aaddee','±è°Ç¸ð','23/01/20','0102223333');
+INSERT INTO cust VALUES ('bbccdd','¹Ì¹Ì¹Ì','23/12/02','0103332221');
+INSERT INTO cust VALUES ('ffddww','¸ð¸ð¸ð','23/01/23','40599493');
 
-INSERT INTO customer VALUES ('mesh153','Á¶¿øÇü','23/01/23','01036493276');
-INSERT INTO customer VALUES ('aaddee','±è°Ç¸ð','23/01/20','0102223333');
-INSERT INTO customer VALUES ('bbccdd','¹Ì¹Ì¹Ì','23/12/02','0103332221');
-INSERT INTO customer VALUES ('ffddww','¸ð¸ð¸ð','23/01/23','40599493');
-select * from customer;
+INSERT INTO cust VALUES ('a1','aaabbbb','23/01/23','01036493276');
+INSERT INTO cust VALUES ('b1','ddddd','23/01/23','01036493276');
+INSERT INTO cust VALUES ('c1','cccccc','23/01/23','01036493276');
+INSERT INTO cust VALUES ('d1','fffff','23/01/23','01036493276');
+select * from cust;
 
-INSERT INTO card (card_id, customer_id, expire, name) VALUES ('999-999-999','mesh153','29/01/23','»ï¼º');
-INSERT INTO card (card_id, customer_id, expire, name) VALUES ('888-888-888','aaddee','33/01/23','·Ôµ¥');
+INSERT INTO card (card_id, expire_date, company_name) VALUES ('123-456-789','29/01/23','»ï¼º');
+INSERT INTO card (card_id, expire_date, company_name) VALUES ('234-567-890','29/01/23','·Ôµ¥');
+INSERT INTO card (card_id, expire_date, company_name) VALUES ('888-888-888','33/01/23','');
 select * from card;
 
-INSERT INTO purchase (id, customer_id, card_id, day, price, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/23', 302000, '±³Åë');
-INSERT INTO purchase (id, customer_id, card_id, day, price, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/25', 34200, 'ºäÆ¼');
-INSERT INTO purchase (id, customer_id, card_id, day, price, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/27', 25000, '½Ä»ç');
+INSERT INTO purchase (id, ssn, card_id, day, price, company_name, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/23', 302000, '»ï¼º', '±³Åë');
+INSERT INTO purchase (id, ssn, card_id, day, price, company_name, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/20', 2000, '»ï¼º', '±³Åë');
+INSERT INTO purchase (id, ssn, card_id, day, price, company_name, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/25', 34200, '»ï¼º', 'ºäÆ¼');
+INSERT INTO purchase (id, ssn, card_id, day, price, company_name, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/05/25', 24200, '»ï¼º', '±â±â');
+INSERT INTO purchase (id, ssn, card_id, day, price, company_name, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/27', 25000, '»ï¼º', '½Ä»ç');
+INSERT INTO purchase (id, ssn, card_id, day, price, company_name, category) VALUES (seq_purchase.nextVal, 'mesh153', '999-999-999', '25/06/23', 302000, '·Ôµ¥', '±³Åë');
 select * from purchase;       
 
+commit;
+
 //cascade test
-delete customer where customer_id = 'mesh153';
-delete company where name = '»ï¼º';
+delete cust where ssn = 'mesh153';
+delete card where card_id = '999-999-999';
